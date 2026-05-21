75184c83c7bc4df1fcf8ff511c7993bc82e0d370
Yan <60644013+yan-alex@users.noreply.github.com>
2026-05-21 14:56:18 +0200

    SET spark_mode = true

--- src/main/config.cpp
+++ src/main/config.cpp
@@ -202,6 +202,7 @@ static const ConfigurationOption internal_options[] = {
     DUCKDB_SETTING(ScalarSubqueryErrorOnMultipleRowsSetting),
     DUCKDB_SETTING(SchedulerProcessPartialSetting),
     DUCKDB_LOCAL(SchemaSetting),
+    DUCKDB_SETTING(SparkModeSetting),
     DUCKDB_LOCAL(SearchPathSetting),
     DUCKDB_GLOBAL(SecretDirectorySetting),
     DUCKDB_SETTING_CALLBACK(StorageBlockPrefetchSetting),
